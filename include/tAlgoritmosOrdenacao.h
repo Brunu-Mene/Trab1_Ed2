7f4088b912e7d18a22aa9679538ae3d2a9aba891
Brunu-Mene <bruno.ufes@hotmail.com>
2022-02-12 15:38:17 -0300

    gg

--- include/tAlgoritmosOrdenacao.h
+++ include/tAlgoritmosOrdenacao.h
@@ -5,13 +5,13 @@
 
 #define TRUE 1
 
-void selectionsort(int *vet, int tam, int T, unsigned long int *comp, unsigned long int *trocas); //
+void selectionsort(int *vet, int tam, int T, unsigned long int *comp, unsigned long int *trocas);
 
 void insertionsort(int *vet, int tam, int T, unsigned long int *comp, unsigned long int *trocas); 
 
 void shellSort(int *vet, int tam, int t,unsigned long int *comp, unsigned long int *trocas);
 
-void quickSort(int *vet, int esq, int dir, unsigned long int *comp, unsigned long int *trocas, int T); //
+void quickSort(int *vet, int esq, int dir, unsigned long int *comp, unsigned long int *trocas, int T);
 
 void heapSort(int *vet, int n, int T , unsigned long int *comp,unsigned long int *trocas);
 
